2b1dd56a1f48ec4e27ee89c882d387beba33bce3
ahmedlotfiali <ahmedlotfiali@gmail.com>
2023-09-10 21:49:08 +0400

    categoty models, list items

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,22 +1,63 @@
+import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _getCategories();
+
     return Scaffold(
       appBar: createAppBar(),
+      backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          generateTextField(),
+          searchField(),
+          const SizedBox(height: 40),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                  color: Colors.green,
+                  height: 300,
+                  child: ListView.builder(
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        return Container();
+                      }))
+            ],
+          )
         ],
       ),
     );
   }
 
-  Container generateTextField() {
+  Container searchField() {
     return Container(
       margin: const EdgeInsets.only(left: 20, right: 30, top: 40, bottom: 10),
       decoration: BoxDecoration(boxShadow: [
@@ -29,10 +70,46 @@ class HomePage extends StatelessWidget {
           decoration: InputDecoration(
               filled: true,
               fillColor: Colors.white,
-              contentPadding: EdgeInsets.all(15),
+              contentPadding: const EdgeInsets.all(15),
+              hintText: 'Search Pancake',
+              hintStyle:
+                  const TextStyle(color: Color(0xffDDDADa), fontSize: 14),
+              prefixIcon: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SvgPicture.asset(
+                    'assets/icons/search.svg',
+                    width: 6,
+                    height: 6,
+                  )),
+              suffixIcon: FilterSuffixIcon(),
               border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(15),
                   borderSide: BorderSide.none))),
+    );
+  }
+
+  Container FilterSuffixIcon() {
+    return Container(
+      width: 100,
+      child: IntrinsicHeight(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const VerticalDivider(
+            color: Color(0xffDDDADA),
+            indent: 10,
+            endIndent: 10,
+            thickness: 0.1,
+          ),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: SvgPicture.asset(
+                'assets/icons/filter-solid.svg',
+                width: 20,
+                height: 20,
+              )),
+        ],
+      )),
     );
   }
 
